af3fe18561c62e6b27295f48d4c5040c0a217691
Eko Setiawan <ekomasterwan993@gmail.com>
2025-01-03 22:41:05 +0700

    add docker operator task, REMEMBER to remove all credentials in the future

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -4,6 +4,7 @@ from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 import requests
+from airflow.providers.docker.operators.docker import DockerOperator
 
 from include.stock_market.tasks import (
     _get_stock_prices,
@@ -46,7 +47,23 @@ def stock_market():
         }
     )
 
-    is_api_available() >> get_stock_prices >> store_prices
+    format_prices = DockerOperator(
+        task_id="format_prices",
+        image="airflow/spark-app",
+        container_name="format_prices",
+        api_version="auto",
+        auto_remove=True,
+        docker_url="tcp://docker-proxy:2375",
+        network_mode="container:spark-master",
+        tty=True,
+        xcom_all=False,
+        mount_tmp_dir=False,
+        environment={
+            "SPARK_APPLICATION_ARGS": '{{ task_instance.xcom_pull(task_ids="store_prices") }}',
+        }
+    )
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices
 
 
 
